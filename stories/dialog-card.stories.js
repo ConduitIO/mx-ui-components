73e528fc71c928d95db51590c62ca63650239149
Brett Goulder <brett.goulder@gmail.com>
2021-03-01 11:12:17 -0800

    Dialog Card

---
+++ stories/dialog-card.stories.js
@@ -0,0 +1,9 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Mxa::DialogCard',
+};
+
+export const Default = () => hbs`<Mxa::DialogCard class="p-8">
+  <div class="font-medium mb-6">Uptime</div>
+</Mxa::DialogCard>`;
