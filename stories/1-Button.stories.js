2dd765a8a9d845405aea27fb02e71f6eb54234bc
Brett Goulder <brett.goulder@gmail.com>
2021-03-01 09:09:22 -0800

    Update Button stories

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -4,15 +4,23 @@ export default {
   title: 'Button',
   argTypes: {
     children: { control: 'text' },
+    buttonType: { control: 'text' },
   },
 };
 
 const Template = (args) => ({
-  template: hbs`<Mxa::Button @buttonType='primary'>Foo</Mxa::Button>`,
+  template: hbs`<Mxa::Button @buttonType={{this.buttonType}}>{{this.children}}</Mxa::Button>`,
   context: args,
 });
 
-export const Text = Template.bind({});
-Text.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   children: 'Button',
+  buttonType: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Button',
+  buttonType: 'secondary',
 };
