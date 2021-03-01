10762ca30e84554cf02a93e2ffbc360acdbbf58c
Brett Goulder <brett.goulder@gmail.com>
2021-03-01 13:40:44 -0800

    Mxa::Input

---
+++ stories/input.stories.js
@@ -0,0 +1,9 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Mxa::Input',
+};
+
+export const Default = () => hbs`<Mxa::Input class="mb-2" disabled={{false}}/>`;
+
+export const Disabled = () => hbs`<Mxa::Input class="mb-2" disabled={{true}}/>`;
