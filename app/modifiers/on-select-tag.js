9c665ebe4e92a2c1d177ce18a4eadb288f28c652
James Martinez <jmar910@gmail.com>
2021-03-08 07:48:53 -0500

    Add mxa/tags-input and add tests
    Also add modifiers this component depends on

---
+++ app/modifiers/on-select-tag.js
@@ -0,0 +1,14 @@
+import { modifier } from "ember-modifier";
+
+export default modifier((element, [callback]) => {
+  function runCallback() {
+    const tagsTextInput = document.querySelector('#tags-input');
+    tagsTextInput.value = '';
+    callback();
+  }
+
+  element.addEventListener('click', runCallback);
+  return () => {
+    element.removeEventListener('', runCallback);
+  };
+});
