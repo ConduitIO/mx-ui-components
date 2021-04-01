048ab6c6120be50940c2f57682e5dd56b0077fda
James Martinez <jmar910@gmail.com>
2021-04-01 13:49:10 -0400

    Add way to pass checked state to toggle

--- addon/components/mxa/toggle.js
+++ addon/components/mxa/toggle.js
@@ -1,4 +1,12 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class MxaToggleComponent extends Component {
+  @tracked
+  isChecked;
+
+  constructor() {
+    super(...arguments);
+    this.isChecked = this.args.isChecked || false;
+  }
 }
