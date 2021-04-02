e30efefa44bd93d54f5c32c377322bce8eecb623
Jesse Jordan <jesse@meroxa.io>
2021-04-02 12:49:14 +0200

    upd(comps): add Mxa:Label component

---
+++ tests/integration/components/mxa/label-test.js
@@ -0,0 +1,38 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | mxa/label', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`
+      <Mxa::Label>
+        Label text
+      </Mxa::Label>
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'Label text');
+  });
+
+  test('it is not marked as required by default', async function(assert) {
+    await render(hbs`
+      <Mxa::Label>
+        Label text
+      </Mxa::Label>
+    `);
+
+    assert.dom('[data-test-label]').doesNotHaveClass('required');
+  });
+
+  test('it is marked visually if set as required', async function(assert) {
+    await render(hbs`
+      <Mxa::Label @isRequired={{true}}>
+        Label text
+      </Mxa::Label>
+    `);
+
+    assert.dom('[data-test-label]').hasClass('required');
+  });
+});
