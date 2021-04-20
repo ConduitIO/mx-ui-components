20974c8afafa10175b584bb6d32b4ad7def25d92
Jesse Jordan <jesse@meroxa.io>
2021-04-20 16:33:21 +0200

    add(stories): add story for font sizes

---
+++ stories/fonts.stories.js
@@ -0,0 +1,42 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+let sizeOptions = ['all', 'xs', 'sm', 'base', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'];
+
+export default {
+  title: 'Fonts',
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: sizeOptions,
+      },
+    },
+  },
+};
+
+const Template = (args) => ({
+  template: hbs`
+  {{#if (eq this.size "all")}}
+    {{#each this.allOptions as |size|}}
+      {{#unless (eq size "all")}}
+      <div class="mt-8">
+        <p class="text-sm text-right text-plum-100">{{concat ".text-" size}}</p>
+        <p class={{concat "text-" size}}>Build enterprise grade data pipelines in minutes not months</p>
+      </div>
+      {{/unless}}
+    {{/each}}
+  {{else}}
+    <div class="mt-8">
+      <p class="text-sm text-right text-plum-100">{{concat ".text-" this.size}}</p>
+      <p class={{concat "text-" this.size}}>Build enterprise grade data pipelines in minutes not months</p>
+    </div>
+  {{/if}}
+  `,
+  context: args,
+});
+
+export const FontSizes = Template.bind({});
+FontSizes.args = {
+  size: 'sm',
+  allOptions: sizeOptions,
+};
