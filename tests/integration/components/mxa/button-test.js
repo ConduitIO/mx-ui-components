18945f1a97c0c02c6eeb3f4ada40522f5d6f9e41
Jesse Jordan <jesse@meroxa.io>
2021-07-30 15:16:30 +0200

    fix(button): always provide content for button elements

--- tests/integration/components/mxa/button-test.js
+++ tests/integration/components/mxa/button-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Integration | Component | mxa/button', function(hooks) {
   setupRenderingTest(hooks);
@@ -34,5 +35,30 @@ module('Integration | Component | mxa/button', function(hooks) {
     await render(hbs`<Mxa::Button @buttonType='danger' />`);
 
     assert.dom('button').hasClass('bg-saffron-100');
+  });
+
+  test('it renders the content on block invocation', async function(assert) {
+    await render(hbs`<Mxa::Button>Upload</Mxa::Button>`);
+
+    assert.dom('button').hasText('Upload');
+  });
+
+  test('it renders text that is passed via the @title argument', async function(assert) {
+    await render(hbs`<Mxa::Button @title="Download" />`);
+
+    assert.dom('button').hasText('Download');
+  });
+
+  test('it renders default text when no content is provided', async function(assert) {
+    await render(hbs`<Mxa::Button />`);
+
+    assert.dom('button').hasText('Submit');
+  });
+
+  test('it is accessible', async function(assert) {
+    await render(hbs`<Mxa::Button />`);
+
+    await a11yAudit();
+    assert.ok(true, 'no a11y detected');
   });
 });
