0ba3a87d2c5353948a4dcb1c713aca5a8703639c
Brett Goulder <brett.goulder@gmail.com>
2021-08-02 08:54:10 -0700

    Add a test for the badge componnent

---
+++ tests/integration/components/mxa/badge-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | mxa/badge', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the badge', async function(assert) {
+    await render(hbs`<Mxa::Badge>Badge text</Mxa::Badge>`);
+
+    assert.equal(this.element.textContent.trim(), 'Badge text');
+  });
+
+  test('it has a default color', async function(assert) {
+    await render(hbs`<Mxa::Badge>Badge text</Mxa::Badge>`);
+
+    assert.dom('[data-test-badge]').hasClass('bg-slate-60');
+  });
+
+  test('it can have a different color', async function(assert) {
+    await render(
+      hbs`<Mxa::Badge @color='bg-saffron-100'>Badge text</Mxa::Badge>`
+    );
+
+    assert.dom('[data-test-badge]').hasClass('bg-saffron-100');
+  });
+});
