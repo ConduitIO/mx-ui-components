f8eb5c60119b2c891150d351a7c097bc336483d7
Jesse Jordan <jesse@meroxa.io>
2021-12-03 12:10:18 +0100

    upd(config): add background colors and container layout option

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,25 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: {
-    default: 'White',
+    default: 'Mute',
     values: [
       {
         name: 'White',
         value: '#ffffff',
       },
+      {
+        name: 'Mute',
+        value: '#FBFBFB',
+      },
+      {
+        name: 'Dark',
+        value: '#111827',
+      },
+      {
+        name: 'Sky',
+        value: '#06B6D4',
+      },
     ],
   },
+  layout: 'centered',
 };
