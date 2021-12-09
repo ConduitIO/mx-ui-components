bd36735a460277c4393221613695a8153efa498e
Jesse Jordan <jesse@meroxa.io>
2021-12-09 13:30:48 +0100

    upd(dialog-card): apply redesign to dialog cards

--- stories/dialog-card.stories.js
+++ stories/dialog-card.stories.js
@@ -4,6 +4,38 @@ export default {
   title: 'Mxa::DialogCard',
 };
 
-export const Default = () => hbs`<Mxa::DialogCard class="p-8">
-  <div class="font-medium mb-6">Uptime</div>
-</Mxa::DialogCard>`;
+const TemplateSimple = (args) => ({
+  template: hbs`<Mxa::DialogCard>
+    <h1>{{this.titleText}}</h1>
+    <h2>{{this.subTitleText}}</h2>
+    <p>{{this.bodyText}}</p>
+  </Mxa::DialogCard>`,
+  context: args,
+});
+
+const TemplateExtended = (args) => ({
+  template: hbs`<Mxa::DialogCard>
+    <:title>{{this.titleText}}</:title>
+    <:sub-title>{{this.subTitleText}}</:sub-title>
+    <:body>
+      <p>{{this.bodyText}}</p>
+    </:body>
+  </Mxa::DialogCard>`,
+  context: args,
+});
+
+export const Default = TemplateExtended.bind({});
+Default.args = {
+  titleText: 'Dialog Card',
+  subTitleText: 'A subtitle header',
+  bodyText: 'This is a dialog card using the extended yield syntax',
+  isContainer: true,
+};
+
+export const Simple= TemplateSimple.bind({});
+Simple.args = {
+  titleText: 'Fullscreen',
+  subTitleText: 'Layout (fullscreen)',
+  bodyText: 'This is a dialog card using the simple yield syntax',
+  isContainer: true,
+};
