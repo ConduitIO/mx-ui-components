391cd61e80063106a91bddf93b1dad042c5aed11
Jesse Jordan <jesse@meroxa.io>
2022-02-23 14:11:48 +0100

    upd(input): allow labelling of mox::input

--- stories/mox-input.stories.js
+++ stories/mox-input.stories.js
@@ -7,13 +7,44 @@ export default {
       default: 'Dark',
     },
   },
+  inputAction: () => {},
 };
 
-export const Default = () =>
-  hbs`<Mox::Input class="mb-2" disabled={{false}} @value="Sun" />`;
+const Template = (args) => ({
+  template: hbs`
+  <Mox::Input class="mb-2" @value={{this.value}} @onInput={{this.inputAction}} @placeholder={{this.placeholder}} @label={{this.label}} disabled={{this.isDisabled}} @isRequired={{this.isRequired}} />`,
+  context: args,
+});
 
-export const Disabled = () =>
-  hbs`<Mox::Input class="mb-2" disabled={{true}} @value="Rain" />`;
+export const Default = Template.bind({});
+Default.args = {
+  value: 'Sun',
+  placeholder: 'Name',
+  label: 'Name',
+  inputAction: () => {},
+};
 
-export const Placeholder = () =>
-  hbs`<Mox::Input class="mb-2" @placeholder="Your Name" />`;
+export const Disabled = Template.bind({});
+Disabled.args = {
+  value: 'Rain',
+  isDisabled: true,
+  label: 'Name',
+  inputAction: () => {},
+};
+
+export const Placeholder = Template.bind({});
+Placeholder.args = {
+  value: '',
+  placeholder: 'Your Name',
+  label: 'Name',
+  inputAction: () => {},
+};
+
+export const Required = Template.bind({});
+Required.args = {
+  value: 'Neptune',
+  placeholder: 'Your Name',
+  label: 'Name',
+  isRequired: true,
+  inputAction: () => {},
+};
