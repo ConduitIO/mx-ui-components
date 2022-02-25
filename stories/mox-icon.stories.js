166f97ddc667cb6c3e54fc966b152027e77606ae
Jesse Jordan <jesse@meroxa.io>
2022-02-25 14:31:02 +0100

    feat(icon): add mox::icon component

---
+++ stories/mox-icon.stories.js
@@ -0,0 +1,45 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+let colorOptions = ['text-white', 'text-gray-300', 'text-red-500', 'text-cyan-500'];
+
+export default {
+  title: 'Mox/Mox::Icon',
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+        options: colorOptions,
+      },
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'Dark',
+    },
+  },
+};
+
+const Template = (args) => ({
+  template: hbs`
+    <div class="{{this.color}} flex flex-col space-y-4">
+      <Mox::Icon @iconName="connectors-16" @size={{this.size}} />
+      <Mox::Icon @iconName="pipelines-16" @size={{this.size}} />
+      <Mox::Icon @iconName="settings-16" @noStroke={{true}} @size={{this.size}} />
+      <Mox::Icon @iconName="search-16" @noStroke={{true}} @size={{this.size}} />
+    </div>`,
+  context: args,
+});
+
+export const Default = Template.bind({});
+export const Large = Template.bind({});
+
+Default.args = {
+  allOptions: colorOptions,
+  color: 'text-gray-300',
+};
+
+Large.args = {
+  allOptions: colorOptions,
+  color: 'text-gray-300',
+  size: 'large',
+};
