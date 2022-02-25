5160a7d224bf5714652e70d4717b66f161ec3273
Jesse Jordan <jesse@meroxa.io>
2022-02-25 14:49:57 +0100

    feat(form-field): add mox::form-field component

---
+++ stories/mox-form-field.stories.js
@@ -0,0 +1,49 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Mox/Mox::FormField',
+  parameters: {
+    backgrounds: {
+      default: 'Dark',
+    },
+  },
+};
+
+const Template = (args) => ({
+  template: hbs`
+  <div class={{this.containerClass}}>
+    <Mox::FormField @id="form-field-example">
+      <:icon>
+        <Mox::Icon @iconName="connectors-16" class="text-white" />
+      </:icon>
+      <:title>
+        Encrypt connecting with SSL/TLS
+      </:title>
+      <:description>
+        We recommend encrypting connections to your Resources.
+      </:description>
+      <:controls>
+        <Mox::Toggle
+          @id="form-field-example"
+          @toggleAction={{this.toggleAction}}
+        />
+      </:controls>
+    </Mox::FormField>
+  </div>
+`,
+  context: args,
+});
+
+export const ToggleFormFieldFullWidth = Template.bind({});
+ToggleFormFieldFullWidth.args = {
+  toggleAction: () => {},
+};
+ToggleFormFieldFullWidth.parameters = {
+  layout: 'fullscreen',
+};
+
+export const ToggleFormFieldInContainer = Template.bind({});
+ToggleFormFieldInContainer.args = {
+  toggleAction: () => {},
+  containerClass: 'w-container',
+};
---
+++ tests/integration/components/mox/form-field-test.js
@@ -0,0 +1,67 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Integration | Component | mox/form-field', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function() {
+    this.set('toggleAction', () => {});
+  });
+
+  test('it renders', async function(assert) {
+    await render(hbs`
+      <Mox::FormField @id="form-field-example">
+        <:icon>
+          replace me with an icon
+        </:icon>
+        <:title>
+          Encrypt connecting with SSL/TLS
+        </:title>
+        <:description>
+          We recommend encrypting connections to your Resources.
+        </:description>
+        <:controls>
+          <Mox::Toggle
+            @id="form-field-example"
+            @toggleAction={{this.toggleAction}}
+          />
+        </:controls>
+      </Mox::FormField>
+    `);
+
+    assert.dom('[data-test-mox-form-field]').includesText('replace me with an icon');
+    assert.dom('[data-test-mox-form-field]').includesText('Encrypt connecting with SSL/TLS');
+    assert.dom('[data-test-mox-form-field]').includesText('We recommend encrypting');
+  });
+
+  test('it is accessible', async function(assert) {
+    await render(hbs`
+      <div class="bg-gray-900">
+        <Mox::FormField @id="form-field-example">
+          <:icon>
+            replace me with an icon
+          </:icon>
+          <:title>
+            Encrypt connecting with SSL/TLS
+          </:title>
+          <:description>
+            We recommend encrypting connections to your Resources.
+          </:description>
+          <:controls>
+            <Mox::Toggle
+              @id="form-field-example"
+              @toggleAction={{this.toggleAction}}
+            />
+          </:controls>
+        </Mox::FormField>
+      </div>
+    `);
+
+    await a11yAudit();
+    assert.ok(true, 'no a11y detected');
+  });
+});
