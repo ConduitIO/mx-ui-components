1a5c27388307bcec8b6821fdd1ad4bf0596969fa
James Martinez <jmar910@gmail.com>
2022-02-28 09:52:41 -0500

    Add Mox::Select & Mox::Select::Option

---
+++ addon/components/mox/select.js
@@ -0,0 +1,35 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action, get } from '@ember/object';
+
+export default class MxaSelectComponent extends Component {
+  @tracked
+  isShowingOptions = false;
+
+  get optionNameKey() {
+    return this.args.optionNameKey || 'name';
+  }
+
+  get optionValueKey() {
+    return this.args.optionValueKey || 'value';
+  }
+
+  get selectedOptionName() {
+    const optionNameKey = this.optionNameKey;
+    return get(this.args.selectedOption, optionNameKey);
+  }
+
+  @action
+  toggleOptions() {
+    if (this.args.isDisabled) {
+      return;
+    }
+    this.isShowingOptions = !this.isShowingOptions;
+  }
+
+  @action
+  setSelectedOption(option) {
+    this.args.onChange(option);
+    this.isShowingOptions = false;
+  }
+}
---
+++ addon/components/mox/select/option.js
@@ -0,0 +1,35 @@
+import Component from '@glimmer/component';
+import { action, get } from '@ember/object';
+
+export default class MxaSelectOptionComponent extends Component {
+  get optionName() {
+    const optionNameKey = this.args.optionNameKey;
+
+    return get(this.args.option, optionNameKey);
+  }
+
+  get optionValue() {
+    const optionValueKey = this.args.optionValueKey;
+
+    return get(this.args.option, optionValueKey);
+  }
+
+  get optionCategory() {
+    const optionCategoryKey = this.args.optionCategoryKey;
+
+    return optionCategoryKey ? get(this.args.option, optionCategoryKey) : null;
+  }
+
+  get selectedOptionValue() {
+    const optionValueKey = this.args.optionValueKey;
+
+    return get(this.args.selectedOption, optionValueKey);
+  }
+
+  @action
+  setOption(option) {
+    if (!this.args.isDisabled) {
+      this.args.setSelectedOption(option);
+    }
+  }
+}
