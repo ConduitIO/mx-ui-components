477d99f349c331325a0b8f69039472f138f2ba87
Brett Goulder <brett.goulder@gmail.com>
2022-03-02 15:00:42 -0800

    chore: styling improvements

--- stories/mox-global-header.stories.js
+++ stories/mox-global-header.stories.js
@@ -14,7 +14,20 @@ const Template = () => ({
   <div class="w-container">
 	<Mox::GlobalHeader>
     <:env-switcher>
-      <Mox::EnvDropdown />
+      <button type="button" class="text-left text-white focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500 focus:outline-none px-4" >
+        <div class="uppercase text-xs" >Don Data</div>
+        <div class="font-bold flex items-center space-x-1">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>
+        Common
+        </span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+        </svg>
+        </div>
+      </button>
     </:env-switcher>
     <:help-menu>
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
