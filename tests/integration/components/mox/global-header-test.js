330aa21c4f4bf55a617393d9558be9f835700819
Brett Goulder <brett.goulder@gmail.com>
2022-03-03 05:51:02 -0800

    chore: add a test for the global header

---
+++ tests/integration/components/mox/global-header-test.js
@@ -0,0 +1,31 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { hbs } from 'ember-cli-htmlbars';
+import { render, click } from '@ember/test-helpers';
+
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+
+module('Unit | Component | mox/global-header', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the header correctly', async function (assert) {
+    await render(hbs`
+	<Mox::GlobalHeader>
+		<:env-switcher>
+            <div>Env switcher</div>
+		</:env-switcher>
+		<:help-menu>
+            <div>Help menu</div>
+		</:help-menu>
+		<:user-menu>
+            <div>User menu</div>
+		</:user-menu>
+	</Mox::GlobalHeader>
+    `);
+
+    assert.dom('[data-test-global-header]').hasTagName('header');
+    assert.dom('[data-test-global-header]').containsText('Env switcher');
+    assert.dom('[data-test-global-header]').containsText('Help menu');
+    assert.dom('[data-test-global-header]').containsText('User menu');
+  });
+});
