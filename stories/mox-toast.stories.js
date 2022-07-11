2f4f6694805ca35c1bdb4b3b18b05019c070b011
Jesse Jordan <jesse@meroxa.io>
2022-07-11 17:00:16 +0200

    fix(toast): allow long text to break over several lines

--- stories/mox-toast.stories.js
+++ stories/mox-toast.stories.js
@@ -12,7 +12,7 @@ export default {
 const Template = (args) => ({
   template: hbs`
     <Mox::Toast @toastType={{this.toastType}}>
-      <:body>This is a toast. Mmm toast. It is not a buttered toast, it is just a toast.</:body>
+      <:body>{{this.message}}</:body>
       <:controls as |controls|>
         <controls.dismissAction>
           Dismiss
@@ -28,19 +28,29 @@ Default.args = {};
 export const Success = Template.bind({});
 Success.args = {
   toastType: 'success',
+  message: 'This is a toast. Mmm toast. It is not a buttered toast, it is just a toast.',
 };
 
 export const Error = Template.bind({});
 Error.args = {
   toastType: 'error',
+  message: 'This is a toast. Mmm toast. It is not a buttered toast, it is just a toast.',
 };
 
 export const Info = Template.bind({});
 Info.args = {
   toastType: 'info',
+  message: 'This is a toast. Mmm toast. It is not a buttered toast, it is just a toast.',
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
   toastType: 'warning',
+  message: 'This is a toast. Mmm toast. It is not a buttered toast, it is just a toast.',
+};
+
+export const LongText = Template.bind({});
+LongText.args = {
+  toastType: 'success',
+  message: 'MMMMMMMMHHHhHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHMMMMMMMMMMMMmmmmmmmmmmmmmmmmmmmmmm.',
 };
