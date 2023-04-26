3c2f3322ce7b188a10345556af15232ec79f8dc8
Jesse Jordan <jesse@meroxa.io>
2023-04-26 08:54:02 +0200

    deps(ember): upgrade app to ember@4.11

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,28 @@ module.exports = {
   },
   rules: {},
   overrides: [
-  // node files
-  {
-    files: ['./.eslintrc.js', './.prettierrc.js', './.template-lintrc.js', './ember-cli-build.js', './index.js', './testem.js', './blueprints/*/index.js', './config/**/*.js', './tests/dummy/config/**/*.js'],
-    excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
-    parserOptions: {
-      sourceType: 'script'
+    // node files
+    {
+      files: [
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ['plugin:n/recommended'],
     },
     env: {
       browser: false,
