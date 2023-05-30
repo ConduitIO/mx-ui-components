c6121c8d1012ca8d76ac0585ce792c460b788112
Jesse Jordan <jesse@meroxa.io>
2023-05-30 13:31:56 +0200

    config(depr.): update depr. workflow for 4.x warnings

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,42 +1,45 @@
 /* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  /* eslint-enable no-undef */
   workflow: [
-    { handler: 'throw', matchId: 'ember-global' },
-    { handler: 'throw', matchId: 'ember.component.reopen' },
-    { handler: 'throw', matchId: 'ember.built-in-components.import' },
-    { handler: 'throw', matchId: 'ember.built-in-components.reopen' },
-    {
-      handler: 'throw',
-      matchId: 'deprecated-run-loop-and-computed-dot-access',
-    },
+    /* Ember deprecations 4.x */
+    { handler: 'throw', matchId: 'remove-owner-inject' },
+    { handler: 'throw', matchId: 'ember-polyfills.deprecate-assign' },
     { handler: 'throw', matchId: 'implicit-injections' },
-    { handler: 'throw', matchId: 'route-render-template' },
-    { handler: 'throw', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'throw', matchId: 'deprecate-auto-location' },
+    { handler: 'throw', matchId: 'setting-on-hash' },
+    { handler: 'throw', matchId: 'deprecate-ember-error' },
+    { handler: 'throw', matchId: 'ember-string.from-ember-module' },
+    /* Ember CLI deprecations 4.x */
     {
       handler: 'throw',
-      matchId: 'ember.built-in-components.legacy-arguments',
+      matchId: 'ember-cli.blueprint.add-bower-package-to-project',
     },
-    { handler: 'throw', matchId: 'ember-modifier.use-modify' },
-    { handler: 'throw', matchId: 'ember-modifier.no-args-property' },
-    { handler: 'throw', matchId: 'ember-modifier.no-element-property' },
-    { handler: 'throw', matchId: 'autotracking.mutation-after-consumption' },
-    { handler: 'throw', matchId: 'ember-modifier.function-based-options' },
     {
       handler: 'throw',
-      matchId: 'ember-keyboard.keyboard-first-responder-on-focus-mixin',
+      matchId: ' ember-cli.blueprint.add-bower-packages-to-project',
+    },
+    { handler: 'throw', matchId: 'ember-cli.building-bower-packages' },
+    { handler: 'throw', matchId: 'ember-cli.project.bower-dependencies' },
+    { handler: 'throw', matchId: 'ember-cli.project.bower-directory' },
+    {
+      handler: 'throw',
+      matchId: 'ember-cli.blacklist-whitelist-build-options',
+    },
+    { handler: 'throw', matchId: 'ember-cli.ember-cli-jshint-support' },
+    { handler: 'throw', matchId: 'ember-cli.vendor-shim-blueprint' },
+    /* Ember Data deprecations 4.x */
+    { handler: 'silence', matchId: 'ember-data:deprecate-array-like' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-early-static' },
+    {
+      handler: 'silence',
+      matchId: 'ember-data:deprecate-non-strict-relationships',
     },
     {
       handler: 'silence',
-      matchId: 'ember-data:method-calls-on-destroyed-store',
+      matchId: 'ember-data:deprecate-promise-many-array-behaviors',
     },
-    { handler: 'throw', matchId: 'ember-keyboard.first-responder-inputs' },
-    { handler: 'throw', matchId: 'ember-keyboard.ember-keyboard-mixin' },
-    { handler: 'throw', matchId: 'manager-capabilities.modifiers-3-13' },
-    { handler: 'throw', matchId: 'autotracking.mutation-after-consumption' },
-    { handler: 'throw', matchId: 'this-property-fallback' },
-    { handler: 'throw', matchId: 'routing.transition-methods' },
-    { handler: 'throw', matchId: 'ember-data:method-calls-on-destroyed-store' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-store-find' },
+    { handler: 'silence', matchId: 'ember-data:deprecate-promise-proxies' },
   ],
 };
