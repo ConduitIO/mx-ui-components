77f90bc58fbc8a8b843a796cf0a1adf3560b043e
James Martinez <jmar910@gmail.com>
2024-03-05 10:46:07 -0500

    fix: test coverage

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -5,6 +5,15 @@ import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import { setupGlobalA11yHooks } from 'ember-a11y-testing/test-support';
+import {
+  forceModulesToBeLoaded,
+  sendCoverage,
+} from 'ember-cli-code-coverage/test-support';
+
+QUnit.done(async function () {
+  forceModulesToBeLoaded();
+  await sendCoverage();
+});
 
 setApplication(Application.create(config.APP));
 setupGlobalA11yHooks(() => true);
